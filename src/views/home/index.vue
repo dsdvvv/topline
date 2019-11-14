2ef9724bb093807b2781677117c6fc57eeed81e8
dsdvvv <2473378640@qq.com>
2019-11-14 18:51:33 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,11 +5,43 @@
     <!-- 整体布局: -->
     <el-container>
       <!-- 左侧内容 -->
-      <el-aside>Aside</el-aside>
+      <el-aside class="left" style="width: 230px">
+        <div class="menu-title" style="background-color: #2e2f32">
+          <img src="../../assets/img/logo_admin.png" alt />
+        </div>
+        <el-menu
+          style="width:231px"
+          background-color="#353b4e"
+          text-color="#adafb5"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 -->
+          <el-menu-item>首页</el-menu-item>
+          <!-- 二级菜单 -->
+          <el-submenu>
+            <template slot="title">内容管理</template>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+          </el-submenu>
+          <!-- 二级菜单 -->
+          <el-submenu>
+            <template slot="title">粉丝管理</template>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+          </el-submenu>
+          <!-- 一级菜单 -->
+          <el-menu-item>账户信息</el-menu-item>
+        </el-menu>
+      </el-aside>
       <!-- 右侧内容 -->
       <el-container>
         <el-header>Header</el-header>
-        <el-main>Main
+        <el-main>
+          Main
           <!-- 二级路由容器 -->
           <router-view></router-view>
         </el-main>
@@ -24,3 +56,17 @@ export default {
   components: {}
 }
 </script>
+<style lang="less" scoped>
+.left {
+  background-color: #353b4e;
+  height: 100vh;
+  overflow: hidden;
+  .menu-title {
+    text-align: center;
+    padding: 20px 0;
+    img {
+      height: 30px;
+    }
+  }
+}
+</style>
