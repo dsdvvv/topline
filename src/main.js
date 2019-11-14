6bd31af641ca6d8cb7927b0554144da7fa5b8f10
dsdvvv <2473378640@qq.com>
2019-11-14 17:24:53 +0800

    黑马头条PC-主页模块-页面布局

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './styles/index.less' // 引入样式
 import axios from 'axios'
 Vue.prototype.$axios = axios // 将axios共享给所有实例使用
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 设置axios的常态地址
-Vue.use(ElementUI)
+Vue.use(ElementUI) // 注册整个elementUI组件 Vue.js调用了elementUI里面的install方法(调用时传入了Vue对象)
 
 Vue.config.productionTip = false
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    <h3>主页</h3>
+    <!-- <h3>主页</h3> -->
+    <!-- 页面布局:左右布局 -->
+    <!-- 整体布局: -->
+    <el-container>
+      <!-- 左侧内容 -->
+      <el-aside>Aside</el-aside>
+      <!-- 右侧内容 -->
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main
+          <!-- 二级路由容器 -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
